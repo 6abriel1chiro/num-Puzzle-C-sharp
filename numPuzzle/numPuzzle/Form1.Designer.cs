7dbe0b422718cbe2b45d73c6ef8d4a42f80e3ee9
Gabriel Ichiro <gabriel582084@gmail.com>
2020-10-28 22:10:46 -0400

    archivos
    protecto terminado y funcional
    //TODO implementar abstraccion a una clase

---
+++ numPuzzle/numPuzzle/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace numPuzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lblNoOfMoves = new System.Windows.Forms.Label();
+            this.createGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.Controls.Add(this.button1);
+            this.pnl.Controls.Add(this.button2);
+            this.pnl.Controls.Add(this.button3);
+            this.pnl.Controls.Add(this.button4);
+            this.pnl.Controls.Add(this.button5);
+            this.pnl.Controls.Add(this.button6);
+            this.pnl.Controls.Add(this.button7);
+            this.pnl.Controls.Add(this.button8);
+            this.pnl.Controls.Add(this.button9);
+            this.pnl.Location = new System.Drawing.Point(196, 108);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(183, 188);
+            this.pnl.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(16, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(71, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = " ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(126, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = " ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(16, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 44);
+            this.button4.TabIndex = 5;
+            this.button4.Text = " ";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(71, 80);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 44);
+            this.button5.TabIndex = 6;
+            this.button5.Text = " ";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(126, 80);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 44);
+            this.button6.TabIndex = 7;
+            this.button6.Text = " ";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.Location = new System.Drawing.Point(16, 130);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(49, 44);
+            this.button7.TabIndex = 8;
+            this.button7.Text = " ";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button8.Location = new System.Drawing.Point(71, 130);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(49, 44);
+            this.button8.TabIndex = 9;
+            this.button8.Text = " ";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button9.Location = new System.Drawing.Point(126, 130);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(49, 44);
+            this.button9.TabIndex = 10;
+            this.button9.Text = " ";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.swapLabel);
+            // 
+            // lblNoOfMoves
+            // 
+            this.lblNoOfMoves.AutoSize = true;
+            this.lblNoOfMoves.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lblNoOfMoves.Location = new System.Drawing.Point(232, 314);
+            this.lblNoOfMoves.Name = "lblNoOfMoves";
+            this.lblNoOfMoves.Size = new System.Drawing.Size(121, 13);
+            this.lblNoOfMoves.TabIndex = 0;
+            this.lblNoOfMoves.Text = "Numero de Movimientos";
+            // 
+            // createGame
+            // 
+            this.createGame.BackColor = System.Drawing.Color.Black;
+            this.createGame.Location = new System.Drawing.Point(220, 59);
+            this.createGame.Name = "createGame";
+            this.createGame.Size = new System.Drawing.Size(159, 23);
+            this.createGame.TabIndex = 1;
+            this.createGame.Text = "Nuevo juego";
+            this.createGame.UseVisualStyleBackColor = false;
+            this.createGame.Click += new System.EventHandler(this.createGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.Location = new System.Drawing.Point(268, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PUZZLE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(471, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createGame);
+            this.Controls.Add(this.lblNoOfMoves);
+            this.Controls.Add(this.pnl);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Mi Puzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lblNoOfMoves;
+        private System.Windows.Forms.Button createGame;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ numPuzzle/numPuzzle/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numPuzzle
+{
+
+
+    class Puzzle
+    {
+
+
+
+    }
+
+    public partial class Form1 : Form
+    {
+
+        Puzzle game;
+        public Form1()
+        {
+
+            game = new Puzzle();
+            InitializeComponent();
+            label2.Text = "0 segundos";
+
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            label2.Text =sw.Elapsed.Seconds.ToString() + " segundos";
+            Application.DoEvents();
+        }
+
+
+        int movesNumber = 0, labelIndex = 0;
+        Timer timer;
+        Stopwatch sw;
+
+        private void shuffleButtons()
+        {
+            if (label2.Text != "0 segundos")
+            {
+                timer.Stop();
+                sw.Stop();
+                label2.Text = "0 segundos";
+
+            }
+           
+
+
+            List<int> labelList = new List<int>(); // 1 , 2, 3 ,4 ,5, 6,7 ,8, white
+
+            Random rand = new Random();
+            foreach (Button btn in this.pnl.Controls) // == for(int i=0:i<9;i++)
+            {
+                while (labelList.Contains(labelIndex))
+                    labelIndex = rand.Next(9); //asigna un numero entre 1 y 9
+
+                btn.Text = (labelIndex == 0) ? "" : labelIndex + ""; //if index = 0  index="" else index + "" 
+
+                btn.BackColor = (btn.Text == "") ? Color.White : Color.FromKnownColor(KnownColor.ControlLight); //if n =="" color to white else color normal
+                labelList.Add(labelIndex); //se llena la lista con el index
+            }
+
+            movesNumber = 0;
+            lblNoOfMoves.Text = "Movimientos : " + movesNumber;
+        }
+     
+
+        private void swapLabel(Object sender, EventArgs e)
+        {
+            if (label2.Text == "0 segundos")
+            {
+                timer = new Timer();
+                timer.Interval = (1000);
+                timer.Tick += new EventHandler(timer_Tick);
+                sw = new Stopwatch();
+                timer.Start();
+                sw.Start();
+            }
+
+            Button btn = (Button)sender;
+            if (btn.Text == "")
+                return;
+
+            Button whiteBtn = null;
+            foreach (Button bt in this.pnl.Controls)
+            {
+                if (bt.Text == "")
+                {
+                    whiteBtn = bt;
+                    break;
+                }
+            }
+
+            if (btn.TabIndex == (whiteBtn.TabIndex - 1) ||  btn.TabIndex == (whiteBtn.TabIndex - 3) || btn.TabIndex == (whiteBtn.TabIndex + 1) || btn.TabIndex == (whiteBtn.TabIndex + 3))
+            {
+                whiteBtn.BackColor = Color.FromKnownColor(KnownColor.ControlLight); //white pasa a color normal
+                btn.BackColor = Color.White;// color notmal pas a white
+                whiteBtn.Text = btn.Text; //swap
+                btn.Text = ""; //swap
+                movesNumber++;
+                lblNoOfMoves.Text = "movimientos : " + movesNumber;
+            }
+
+            checkOrder();
+        }
+
+        private void checkOrder()
+        {
+            int index = 1;
+            foreach (Button btn in this.pnl.Controls)
+            {
+                if (btn.Text != "" && Convert.ToInt16(btn.Text) != index)
+                {
+                    return;
+
+                }
+
+                index++;
+            }
+            timer.Stop();
+            sw.Stop();
+            MessageBox.Show("Has ganado en  " + movesNumber + " movimientos" );
+            label2.Text = "completado en " + sw.Elapsed.Seconds.ToString() + " segundos";
+
+        }
+
+        private void createGame_Click(object sender, EventArgs e)
+        {
+
+
+            movesNumber = 0;
+            labelIndex = 0;
+            shuffleButtons();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            shuffleButtons();
+        }
+    }
+
+}
+   
